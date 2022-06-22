0c061155bcfa0e28b605dac99add4a094faece7f
hung nguyenxuan <hung6nuh@gmail.com>
2022-06-23 01:09:59 +0700

    HUNGNX commit title

--- capstone2022/src/app/components/pages/classify-page/title-category-page/title-category-page.component.ts
+++ capstone2022/src/app/components/pages/classify-page/title-category-page/title-category-page.component.ts
@@ -2,28 +2,210 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { TitleService } from 'src/app/services/title-service/title.service';
-
+import { CommonService } from 'src/app/services/common.service';
+import Swal from 'sweetalert2';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-title-category-page',
   templateUrl: './title-category-page.component.html',
   styleUrls: ['./title-category-page.component.scss'],
 })
 export class TitleCategoryPageComponent implements OnInit {
+
+
+  disable = true;
+  idSelected: any;
   route = { name: 'Title categories', link: 'phanloaitochuc' };
-  constructor(private fb: FormBuilder,private titleservice:TitleService) {}
   titleForm!: FormGroup;
-  titleList:any
+  action = 'Viewing';
+  categories: any;
+  listSelected!: Array<number>;
+  selectedIndex = 0;
+  code = 'RC_PROJECT';
+  itemsPerPage = 20;
+  totalItems!: number;
+  page: number = 1;
+  selectedIndexInTable: any;
+
+
+  constructor(private fb: FormBuilder, private titleservice: TitleService,
+    private commonService: CommonService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
+
+  titleList: any
   ngOnInit(): void {
+    this.page = this.activatedRoute.snapshot.queryParams["index"];
+    this.itemsPerPage = this.activatedRoute.snapshot.queryParams["size"];
     this.titleForm = this.fb.group({
-      name: [{ value: '', disabled: false }],
-      code: [{ value: '', disabled: false }],
-      note: [{ value: '', disabled: false }],
+      name: [{ value: '', disabled: true }],
+      code: [{ value: '', disabled: true }],
+      note: [{ value: '', disabled: true }],
     });
-    this.titleservice.getAll(0,3).subscribe((response:any)=>{
-      this.titleList=response.data
+    this.listSelected = []
+    this.titleservice.getAll(0, 9999).subscribe((response: any) => {
+      this.titleList = response.data
     })
   }
-  onSubmit(){
+  addNewTitle() {
+    this.resetValue();
+    this.enableAllControl();
+    this.disable = false;
+    this.action = 'Add new';
+    this.selectedIndexInTable = null;
+    this.innitCode();
+  }
+  innitCode() {
+    this.commonService
+      .autoGencode3Char('Title', 'CD')
+      .subscribe((res: any) => {
+        this.titleForm.controls['code'].setValue(res);
+      });
+  }
+  enableAllControl() {
+    this.titleForm.controls['name'].enable();
+    this.titleForm.controls['note'].enable();
+  }
+  disableControl() {
+    this.titleForm.controls['name'].disable();
+    this.titleForm.controls['note'].disable();
+  }
+  resetValue() {
+    this.titleForm.controls['name'].setValue('');
+    this.titleForm.controls['note'].setValue('');
+    this.titleForm.controls['code'].setValue('');
+  }
+  onSubmit() {
+    let obj = {
+      id: 0,
+      code: this.titleForm.controls['code'].value,
+      name: this.titleForm.controls['name'].value,
+      note: this.titleForm.controls['note'].value,
+      status: -1
+    };
+    if (this.action == 'Add new') {
+      this.titleservice.insertTitle(obj).subscribe(
+        (res: any) => {
+          if (res.status == true) {
+            this.loadData(this.page - 1);
+            this.resetValue();
+            this.disableControl();
+            this.innitCode();
+            this.commonService.popUpSuccess();
+          } else {
+            this.commonService.popUpFailed('Failed');
+          }
+        },
+        (err) => {
+          this.commonService.popUpFailed('Failed');
+        }
+      );
+    }
 
+    if (this.action == 'Editting') {
+      let obj = {
+        id: this.idSelected,
+        code: this.titleForm.controls['code'].value,
+        name: this.titleForm.controls['name'].value,
+        note: this.titleForm.controls['note'].value,
+      };
+      this.titleservice.modifyTitle(obj).subscribe(
+        (res: any) => {
+          console.log(res);
+          if (res.status == true) {
+            this.loadData(this.page - 1);
+            this.resetValue();
+            this.disableControl();
+            this.commonService.popUpSuccess();
+          } else {
+            this.commonService.popUpFailed('Failed');
+          }
+        },
+        (err) => {
+          this.commonService.popUpFailed('Failed');
+        }
+      );
+    }
+  }
+  renderData(index: number, categoryID: number) {
+    this.page = 1;
+    this.resetValue();
+    this.resetSelectedList();
+    this.selectedIndex = index;
+    this.selectedIndexInTable = null;
+    this.loadData(this.page - 1);
+    this.disableControl();
+  }
+  loadData(pageIndex: number) {
+    this.titleservice
+      .getAll(pageIndex, this.itemsPerPage)
+      .subscribe((res: any) => {
+        this.titleList = res.data;
+        this.totalItems = res.totalItem;
+      });
+  }
+  chooseItem(item: any, i: number) {
+    this.selectedIndexInTable = i;
+    this.enableAllControl();
+    this.action = 'Editting';
+    this.disable = false;
+    this.idSelected = item.id;
+    this.titleForm.controls['name'].setValue(item.name);
+    this.titleForm.controls['note'].setValue(item.note);
+    this.titleForm.controls['code'].setValue(item.code);
+  }
+
+
+  resetSelectedList() {
+    this.listSelected = [];
+  }
+
+  updateAllComplete($event: any, id: number) {
+    if ((event?.target as any).checked) {
+      this.listSelected.push(id);
+    } else {
+      let index = this.listSelected.findIndex((idObject) => idObject == id);
+      this.listSelected.splice(index, 1);
+    }
+    console.log(this.listSelected);
+  }
+  gty(page: number) {
+    this.router.navigateByUrl(`/phanloaitochuc/danhmuchucdanh?index=${page - 1}&size=${this.itemsPerPage}`);
+    this.resetValue();
+    this.selectedIndexInTable = null;
+    this.loadData(page - 1);
+  }
+  deleteTitle() {
+    if (this.listSelected.length <= 0) {
+      this.commonService.popUpMessage('Choose at least one record!!!');
+    } else {
+      Swal.fire({
+        text: 'Are you sure to delete?',
+        iconHtml:
+          ' <img src="../../../assets/images/icons/ques.jpg" width="100px" alt="">',
+        showCancelButton: true,
+        confirmButtonColor: '#309EFC',
+        cancelButtonColor: '#8B94B2',
+        confirmButtonText: 'Confirm',
+        width: '380px',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.titleservice.deleteTitle(this.listSelected).subscribe(
+            (res: any) => {
+              if (res.status == true) {
+                this.loadData(this.page - 1);
+                this.commonService.popUpSuccess();
+                this.listSelected = []
+              } else {
+                this.commonService.popUpFailed('Some records have been appplied');
+              }
+            },
+            (err) => {
+              this.commonService.popUpFailed('Some records have been appplied');
+            }
+          );
+        }
+      });
+    }
   }
 }
--- capstone2022/src/app/services/title-service/title.service.ts
+++ capstone2022/src/app/services/title-service/title.service.ts
@@ -6,10 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class TitleService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getAll(index:number,size:number){
-    return this.http.post('api',{index,size})
+  getAll(index: number, size: number) {
+    return this.http.post(`https://localhost:44376/api/OrgnizationAPI/GetAllTitle?index=${index}&size=${size}`, {})
   }
-  
+
+  insertTitle(request: any) {
+    return this.http.post(
+      'https://localhost:44376/api/OrgnizationAPI/InsertTitle',
+      request
+    );
+  }
+  modifyTitle(request: any) {
+    return this.http.post(
+      'https://localhost:44376/api/OrgnizationAPI/ModifyTitle',
+      request
+    );
+  }
+
+  deleteTitle(arr: Array<number>) {
+    return this.http.post(
+      'https://localhost:44376/api/OrgnizationAPI/DeleteTitle',
+      arr
+    );
+  }
 }
